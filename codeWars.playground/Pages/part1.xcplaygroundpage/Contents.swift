3a7dd07699a505f855592779e6efb92f3c127b80
PanAndrei <andrei_kmv@inbox.ru>
2021-10-26 13:56:54 +0300

    Initial Commit

---
+++ codeWars.playground/Pages/part1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,397 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+
+/// don't give me five
+
+//func doNotGive5(_ a: Int, _ b: Int) -> Int {
+//    var count : Int = 0
+//    var str : String
+//    for i in a...b {
+//       str = String(i)
+//        if str.contains("5") {
+//            continue
+//        } else {
+//            count += 1
+//        }
+//    }
+//    return count
+//}
+//
+//doNotGive5(-1, 3)
+//
+//func dontGiveMeFive(_ start: Int, _ end: Int) -> Int {
+//  return (start...end).filter { !String($0).contains("5") }.count
+//}
+//
+//dontGiveMeFive(2, 61)
+
+// missing letter
+
+
+//func findMissingLetter (_ cgArr: [Character]) -> Character {
+//    var temp : Int
+//    var needToBefind : Character = "1"
+//    let charMin : Array <Character> = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//    let charMax : Array <Character> = ["A", "B", "C", "D", "E", "F", "G", "H", "I" , "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//
+//    if charMin.contains(cgArr[0]) {
+//        for j in 0 ... charMin.count - 1 {
+//            if charMin[j] != cgArr[0] {
+//                continue
+//            } else {
+//                temp = j
+//                for x in 0 ... cgArr.count - 1 {
+//                    if cgArr[x] == charMin[x + temp] {
+//                        continue
+//                    } else {
+//                        needToBefind = charMin[x + temp]
+//                        break
+//                    }
+//                }
+//            }
+//        }
+//    } else {
+//        for j in 0 ... charMax.count - 1 {
+//            if charMax[j] != cgArr[0] {
+//                continue
+//            } else {
+//                temp = j
+//                for x in 0 ... cgArr.count - 1 {
+//                    if cgArr[x] == charMax[x + temp] {
+//                        continue
+//                    } else {
+//                        needToBefind = charMax[x + temp]
+//                        break
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    return needToBefind
+//}
+//
+//let che : Array <Character> = ["b", "d", "f", "e"]
+//findMissingLetter(che)
+//
+//
+//func findMissingLetter(_ chArr: [Character]) -> Character {
+//  for char in chArr {
+//    let next = Character(UnicodeScalar(char.asciiValue! + 1))
+//    if !chArr.contains(next) { return next }
+//  }
+//  return Character("")
+//}
+
+
+// mask for credit card
+
+
+//
+//func mask (_ string : String) -> String {
+//    var newArr : Array <Character> = []
+//    var ele : Character
+//    var str = ""
+//    for element in string {
+//        newArr.append(element)
+//    }
+//    if newArr.count > 4 {
+//        for i in 0 ... newArr.count - 5 {
+//            newArr[i] = "#"
+//        }
+//        for element in newArr {
+//            str += String(element)
+//        }
+//    } else {
+//        str = string
+//    }
+//    return str
+//}
+//
+//func maskify3(_ string:String) -> String {
+//  let last4Index = string.count - 4
+//  let maskedString = String(string.enumerated().map { (index, element) -> Character in
+//    return index < last4Index ? "#" : element
+//  })
+//  return maskedString
+//}
+//
+//func maskify4(_ string:String) -> String {
+//  string.enumerated().map { $0 < string.count - 4 ? "#" : "\($1)"}.joined()
+//}
+//
+//mask("yymnljkjkhvbkvnmy")
+
+//var a : Character = "4"
+//var b = 0
+//b = Int(a) ?? 0
+
+// pow every number
+
+//func squareDigits (_ num: Int) -> Int {
+//    var resultInt = 0
+//    var arrayStr : String = ""
+//    var strInt = String(num)
+////    var newStrInt = strInt.map({(value: Character) -> Int in
+////        return Int(value) * Int(value)})
+//    for element in strInt {
+//        let res = String(element)
+//        let resint = Int(res) ?? 0
+//        arrayStr += String(resint * resint)
+//
+//    }
+//    resultInt = Int(arrayStr) ?? 0
+//    return resultInt
+//}
+//
+//func squareDi4gits(_ num: Int) -> Int {
+//    Int(String(num).compactMap() {
+//        String(Int(pow(Double(String($0))!, 2)))
+//    }.joined())!
+//}
+//
+//squareDigits(56565)
+
+// changing weights
+//https://www.codewars.com/kata/55c6126177c9441a570000cc/train/swift
+
+//var str = "19 12 44"
+//
+//var arr2 = [2, 4, 6, 7]
+//var arr3 = [3, 5, 88, 3]
+//
+//let arr32 = zip(arr2, arr3)
+//let dict2 = Dictionary(uniqueKeysWithValues: arr32)
+//
+//
+//func orderWeight(_ s: String) -> Dictionary<Int, Int>.Element {
+//    ///kostyl
+//    let newS = s + " "
+//    var numStr : String = ""
+//    var newInt: Int = 0
+//    var strFinal = ""
+//    var arrayOfint : Array <Int> = []
+//    var arrayOfintSum : Array <Int> = []
+//    var dict : Dictionary <Int, Int> = [:]
+//
+//    for number in newS {
+//        if number != " " {
+//            numStr += String(number)
+//        } else {
+//            arrayOfint.append(Int(numStr) ?? 0)
+//            numStr = ""
+//            continue
+//        }
+//    }
+//
+//    for i in 0 ... arrayOfint.count - 1 {
+//        let strCha = String(arrayOfint[i])
+//        for ele in strCha {
+//            newInt += Int(String(ele)) ?? 0
+//        }
+//        arrayOfintSum.append(newInt)
+//        newInt = 0
+//    }
+//
+//    let tempArray = zip(arrayOfint, arrayOfintSum)
+//
+//    dict = Dictionary(uniqueKeysWithValues: tempArray)
+//    var dictSorted : Dictionary <Int, Int> = [:]
+//      dictSorted = dict.sorted(by: {$0.1 < $1.1})
+//
+//
+//    for element in dict.
+//
+//}
+//
+//let bb = orderWeight(str)
+//print(bb)
+
+
+
+///print("  *\n ***\n*****\n ***\n  *\n")
+/// dimonds printing
+///https://www.codewars.com/kata/5503013e34137eeeaa001648/train/swift
+
+
+//func dimond(_ size: Int?) -> String? {
+//    var num = 1
+//    let charEmpt = " "
+//    let charStar = "*"
+//    let charJump = "\n"
+//    var stringFinal = ""
+//
+//
+//    if size! % 2 == 0 || size! <= 0 {
+//        return nil
+//    } else {
+//        for _ in 1 ... size! {
+//            if num < size! {
+//                for _ in 1 ... ((size! - num) / 2) {
+//                stringFinal += charEmpt
+//            }
+//                for _ in 1 ... num {
+//                    stringFinal += charStar
+//                }
+//                stringFinal += charJump
+//                num += 2
+//            } else if num == size {
+//                for _ in 1 ... size! {
+//                    stringFinal += charStar
+//                }
+//                stringFinal += charJump
+//                num += 2
+//            } else {
+//                for _ in 1 ... -((size! - num) / 2) {
+//                    stringFinal += charEmpt
+//                }
+//                for _ in 1 ... ((size! * 2) - num) {
+//                    stringFinal += charStar
+//                }
+//                stringFinal += charJump
+//                num += 2
+//
+//            }
+//    }
+//
+//
+//}
+//    return stringFinal
+//}
+//
+//print (dimond(17) ?? "")
+
+//func diamond(_ size: Int) -> String? {
+//    guard size > 0 && size % 2 == 1 else {
+//        return nil
+//    }
+//    var diamond = ""
+//    let centerRow = size/2 + 1
+//    for row in 1...size {
+//        let spaces = abs(centerRow - row)
+//        diamond += String(repeating: " ", count: spaces) + String(repeating: "*", count: size-spaces*2) + "\n"
+//    }
+//    return diamond
+//}
+
+//func diamond(_ size: Int) -> String? {
+//  guard size > 0 && size % 2 == 1 else { return nil }
+//  var dmnd = ""
+//  let centerRow = size/2 + 1
+//  (1...size).forEach {
+//    let spc = abs(centerRow - $0)
+//    dmnd += String(repeating: " ", count: spc) + String(repeating: "*", count: size-spc * 2) + "\n"
+//  }
+//  return dmnd
+//}
+
+
+// the walker
+
+//let a = 12.0 * cos(45.0 * Double.pi / 180)
+//let b = 12.0 * sin(45.0 * Double.pi / 180)
+//
+//func solve(_ a: Int, _ b: Int, _ c: Int, _ alpha: Int, _ beta: Int, _ gamma: Int) -> [Int] {
+//    let pi180 = 3.14159265358979323846 / 180.0
+//    var answer = [Int]()
+//    let pointX = Double(a) * cos(Double(alpha) * pi180) - Double(b) * sin(Double(beta) * pi180) - Double(c) * cos(Double(gamma) * pi180)
+//    let pointY = Double(a) * sin(Double(alpha) * pi180) + Double(b) * cos(Double(beta) * pi180) - Double(c) * sin(Double(gamma) * pi180)
+//    let distanceOC = (sqrt(pow(pointX, 2) + pow(pointY, 2)))
+//    let distanceOCy = Int(distanceOC.rounded(.toNearestOrAwayFromZero))
+//    answer.append(distanceOCy)
+//
+//    let tngF = pointY / pointX
+//    let anglF = atan(tngF) / pi180
+//    let angCherch = 180.0 + anglF
+//    let answInMin = 60.0 * (angCherch - Double(Int(angCherch)))
+//    let answInSec = 60.0 * (answInMin - Double(Int(answInMin)))
+//    answer.append(Int(angCherch))
+//    answer.append(Int(answInMin))
+//    answer.append(Int(answInSec))
+//    return answer
+//}
+//
+//solve(15, 15, 19, 50, 29, 55)
+//dotest(15,15,19,50,29,55, [12, 133, 18, 44])
+
+
+// find 3 and 5 multiplies
+
+//func solution (_ num: Int) -> Int {
+//    var answ = 0
+//    if num > 0 {
+//    for i in 0 ... num - 1 {
+//        if i % 3 == 0 || i % 5 == 0 {
+//            answ += i
+//        }
+//    }
+//    return answ
+//    } else {
+//        return 0
+//    }
+//}
+//
+//solution(200)
+//
+//func solutio3n(_ num: Int) -> Int {
+//  return (0..<num).filter { ($0 % 3 * $0 % 5) == 0 }.reduce(0, +)
+//}
+//
+//func solutsion(_ num: Int) -> Int {
+//  var sum = 0
+//  for i in 0..<num {
+//    if (i % 3 == 0) || (i % 5 == 0) {
+//      sum += i
+//    }
+//  }
+//  return num > 0 ? sum : 0
+//}
+
+// calculate averege - perfect
+
+//func calculateAvr (_ marks: [Int]) -> Int {
+//    var avrMark = 0
+//    avrMark = (marks.reduce(0, +) / marks.count)
+//    return avrMark
+//}
+//
+//let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//calculateAvr(arr)
+
+// jumpingNumber
+
+//func jumpingNumber (_ number: Int) -> String {
+//    var strNum = String(number)
+//    var arrNum : Array<Int> = []
+//    var strReturn = ""
+//    for element in strNum {
+//        arrNum.append(Int(String(element))!)
+//    }
+//
+//    if arrNum.count == 1 {
+//        strReturn = "Jumping!!"
+//    } else {
+//        for i in 1 ... arrNum.count - 1 {
+//            if abs(arrNum[i] - arrNum[i - 1]) == 1  {
+//                strReturn = "Jumping!!"
+//            } else {
+//                strReturn = "Not!!"
+//                break
+//            }
+//        }
+//    }
+//    return strReturn
+//}
+//
+//jumpingNumber(89098)
+//
+//func jumpinegNumber(_ number: Int) -> String {
+//  let numbers = String(number).compactMap { c in c.wholeNumberValue };
+//  for i in 1..<numbers.count {
+//      if abs(numbers[i - 1] - numbers[i]) != 1 {
+//          return "Not!!";
+//      }
+//  }
+//  return "Jumping!!";
+//}
---
+++ codeWars.playground/Pages/part2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,389 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
+
+// primal integers
+
+//Int(sqrt(Double(element + g)))
+
+//func gap(_ g: Int, _ m: Int, _ n:Int) -> (Int, Int)? {
+//    var answer : (Int, Int)? = (0, 0)
+//    var arrayI : Array<Int> = []
+//    guard g >= 2, m > 2, n > m else {
+//        print("enter valid numbers")
+//        return nil
+//    }
+//    primal:  for element in m ... n {
+//        var count = 1
+//        for i in 2 ... Int(sqrt(Double(element))) {
+//            count += 1
+// а еще можно через 2 перескакивать тк четность
+//            if (element %  i == 0) {
+//                continue primal
+//            } else if count == Int(sqrt(Double(element))) {
+//                arrayI.append(element)
+//            }
+//        }
+//        count = 2
+//    }
+//    var niva = 0
+//    for j in 0 ... arrayI.count - 2 {
+//        niva = arrayI[j + 1] - arrayI[j]
+//        if niva != g {
+//            niva = 0
+//            continue
+//        } else {
+//            answer?.0 = Int(arrayI[j])
+//            answer?.1 = Int(arrayI[j + 1])
+//            break
+//        }
+//    }
+//    if answer?.0 ?? 0 > 1 {
+//        return answer
+//    } else {
+//        return nil
+//    }
+//}
+//
+//gap(2, 9, 110)
+//
+//func gap3(_ g: Int, _ m: Int, _ n: Int) -> (Int, Int)? {
+//  guard g % 2 == 0 else { return nil }
+//  let primes = (m...n).lazy.filter(isPrime)
+//  return zip(primes, primes.dropFirst()).first(where: { $0.1 - $0.0 == g })
+//}
+//
+//func isPrime(_ n: Int) -> Bool {
+//  return (2...Int(Double(n).squareRoot())).lazy.filter({ n % $0 == 0 }).first == nil
+//}
+
+
+// disarium numbers
+
+//func disariumNumber (_ number: Int) -> String {
+//    let num = String(number)
+//    var arr : Array <Int> = []
+//    for char in num {
+//        arr.append(Int(String(char))!)
+//    }
+//    var check = 0.0
+//    for i in 0 ... arr.count - 1 {
+//        check += pow(Double(arr[i]), Double(i + 1))
+//    }
+//    return Int(check) == number ? "Disarium !!" : "Not !!"
+//}
+//
+//func disarwiumNumber(_ number: Int) -> String {
+//    let numberArr = String(number).compactMap { Int(String($0)) }
+//    let ret = numberArr.enumerated().map { (i, n) in Int(pow(Double(n), Double(i + 1))) }.reduce(0, +)
+//    return ret == number ? "Disarium !!" : "Not !!"
+//}
+//
+//disariumNumber(89)
+//disariumNumber(133)
+
+
+// balanced number
+
+//func balancedNumber(_ number: Int) -> String {
+//    let numberArr = String(number).compactMap { Int(String($0)) }
+//
+//    var left = 0
+//    var right = 0
+//
+//    if numberArr.count == 1 || numberArr.count == 2 {
+//        return "Balanced"
+//    } else if (numberArr.count % 2 == 0) {
+//        for i in 0 ... (numberArr.count / 2) - 2 {
+//            left += numberArr[i]
+//        }
+//        for i in ((numberArr.count / 2) + 1) ... (numberArr.count - 1) {
+//            right += numberArr[i]
+//        }
+//    } else if (numberArr.count % 2 != 0) {
+//        for i in 0 ... ((numberArr.count - 1) / 2) - 1 {
+//            left += numberArr[i]
+//        }
+//        for i in ((numberArr.count + 1) / 2) ... (numberArr.count - 1) {
+//            right += numberArr[i]
+//        }
+//    }
+//   return left == right ? "Balanced" : "Not Balanced"
+//}
+//
+//balancedNumber(1024)
+//balancedNumber(262)
+//
+//func baleancedNumber(_ number: Int) -> String {
+//  let n = String(number).map({ Int(String($0))! })
+//  guard n.count > 2 else { return "Balanced" }
+//  let fn = Array(n[0 ..< n.count/2-(n.count%2 == 0 ? 1 : 0)]).reduce(0, +)
+//  let ln = Array(n[n.count/2+1 ..< n.count]).reduce(0,+)
+//  return (fn != ln ? "Not " : "") + "Balanced"
+//}
+
+// secolo
+
+//func centuryFromYear(year: Int) -> Int {
+//let yearD : Double = Double(year) / 100
+//let secD = yearD.rounded(.up)
+//let secI = Int(secD)
+//return secI
+//}
+//
+//centuryFromYear(year: 1701)
+//
+//func centdduryFromYear(year: Int) -> Int {
+//    return (year + 99) / 100
+//}
+
+// palindrom
+
+//func checkPalindrome(inputString: String) -> Bool {
+//    guard inputString.count > 1 else {
+//        return true
+//    }
+//
+//    var arr : Array <String> = []
+//    for char in inputString {
+//        arr.append(String(char))
+//    }
+//
+//    for i in 0 ... arr.count - 1 {
+//        if arr[i] != arr[(arr.count - 1) - i] {
+//            return false
+//            break
+//        } else {
+//        }
+//    }
+//    return true
+//}
+//
+//checkPalindrome(inputString: "dde")
+//
+//func checkfPalindrome(inputString: String) -> Bool {
+//  return String(inputString.characters.reverse()) == inputString
+//}
+
+// closed elements
+
+//func adjacentElementsProduct(_ arr: [Int]) -> Int {
+//    var answ = Int.min
+//    for i in 0 ... arr.count - 1 {
+//        if i == arr.count - 1 {
+//            return answ
+//        }
+//        if arr[i] * arr[i + 1] > answ {
+//            answ = arr[i] * arr[i + 1]
+//        }
+//    }
+//    return answ
+//}
+//
+//adjacentElementsProduct([1, 3, 5, 3, 7, 9, -1, -9, 9])
+//
+//func adjacentElemenftsProduct(inputArray: [Int]) -> Int {
+//    return (0..<inputArray.count - 1).map { inputArray[$0] * inputArray[$0 + 1] }.max()!
+//}
+
+// n - polygon
+
+//func shapeArea(n: Int) -> Int {
+//    return (n * n) + ((n - 1) * (n - 1))
+//}
+
+
+// status of gifts
+
+//func makeArrayConsecutive2(statues: [Int]) -> Int {
+//    return (statues.max()! - statues.min()!) - statues.count + 1
+//}
+//
+//
+//makeArrayConsecutive2(statues: [6, 2, 3, 8])
+
+// remove one element
+
+//func almostIncreasingSequence(sequence: [Int]) -> Bool {
+//    guard sequence.count > 2 else {
+//        return true
+//    }
+//    if (sequence.count - Set(sequence).count) > 1 {
+//        return false
+//    }
+//
+//
+//    return true
+//}
+//
+//almostIncreasingSequence(sequence: [1, 1, 2, 3, 4, 4])
+
+//2 ≤ sequence.length ≤ 105,
+//-105 ≤ sequence[i] ≤ 105
+
+//var arrc = [Int]()
+//for _ in 1 ... 10_000 {
+//arrc.append(Int.random(in: -100_000 ... 100_000))
+//}
+//let arccLet = arrc
+//almostIncreasingSequence(sequence: arccLet)
+
+/// слишком долго 
+//func almostIncreasingSequence(sequence: [Int]) -> Bool {
+//    guard sequence.count > 2 else {
+//        return true
+//    }
+//    if (sequence.count - Set(sequence).count) > 1 {
+//        return false
+//    }
+//    let newSeq = sequence
+//    for i in 0 ... newSeq.count - 1 {
+//        var tempArr = newSeq
+//        tempArr.remove(at: i)
+//        if tempArr.sorted() == tempArr  {
+//            return true
+//        }
+//        tempArr = newSeq
+//    }
+//    return false
+//}
+
+// над этим подумать
+//func almoffstIncreasingSequence(sequence: [Int]) -> Bool {
+//var strikes = 0, firstMax = -100000, secondMax = -100000
+//
+//for i in 0...sequence.count - 1 {
+//    if sequence[i] > firstMax {
+//        secondMax = firstMax
+//        firstMax = sequence[i]
+//    } else if sequence[i] > secondMax {
+//        firstMax = sequence[i];
+//        strikes += 1
+//    } else {
+//        strikes += 1
+//    }
+//}
+//
+//return strikes < 2
+//}
+//
+//almoffstIncreasingSequence(sequence: arccLet)
+
+/// matrix and gosts
+
+//var matrix = [[0, 1, 1, 2],
+//              [0, 5, 0, 0],
+//              [2, 0, 3, 3]]
+//
+//
+//func matrixElementsSum(matrix: [[Int]]) -> Int {
+//    var matrixnew = matrix
+//    max: for i in 0 ... matrixnew.count - 1 {
+//          for j in 0 ... matrixnew[i].count - 1 {
+//            if matrixnew[i][j] == 0 {
+//                for x in i ... matrixnew.count - 1 {
+//                    matrixnew[x][j] = 0
+//                }
+//            }
+//        }
+//    }
+//    let matrixFlat = matrixnew.flatMap{$0}
+//    return matrixFlat.reduce(0, +)
+//}
+//
+//matrixElementsSum(matrix: matrix)
+
+
+// совсем по другому цикл/ рассмотреть!
+
+//func matrixEleввmentsSum(matrix: [[Int]]) -> Int {
+// var sum = 0
+//    for column in 0..<matrix[0].count {
+//        for row in 0..<matrix.count {
+//            if matrix[row][column] != 0 {
+//                sum += matrix[row][column]
+//            } else {
+//                break
+//            }
+//        }
+//    }
+//    return sum
+//}
+
+/// return longest string
+
+//func allLongestStrings(inputArray: [String]) -> [String] {
+//    var answerArr : Array <String> = []
+//    var lenghOfElement = 0
+//    for element in inputArray {
+//        if element.count > lenghOfElement {
+//            lenghOfElement = element.count
+//        }
+//    }
+//    for element in inputArray {
+//        if element.count == lenghOfElement {
+//            answerArr.append(element)
+//        }
+//    }
+//    return answerArr
+//}
+//
+//func allLongestSfftrings(inputArray: [String]) -> [String] {
+//    let longest = inputArray.map{$0.utf8.count}.maxElement()!
+//    return inputArray.filter{$0.utf8.count == longest}
+//}
+//
+//func allLong444estStrings(inputArray: [String]) -> [String] {
+//    return inputArray.filter { $0.characters.count == inputArray.map { $0.characters.count }.max() }
+//}
+
+/// common characters
+
+//var s1ed =  "abaca", s2 = "abaac"
+//var dfda = Array(s1ed), adf = Array(s2)
+//dfda.sorted() == adf.sorted()
+//
+//func commonCharacterCount(s1: String, s2: String) -> Int {
+//    var count = 0
+//    var arr1 = Array(s1)
+//    var arr = Array(s2)
+//    if arr1.sorted() == arr.sorted() {
+//        return arr.count
+//    }
+//    nest:  for element in arr1 {
+//        for i in 0 ... arr.count - 1  {
+//            if element == arr[i] {
+//                arr.remove(at: i)
+//                count += 1
+//                if arr.count - 1 > 0 {
+//                    continue nest
+//                } else {
+//                    break nest
+//                }
+//            }
+//        }
+//    }
+//    return count
+//}
+//
+//commonCharacterCount(s1: "abaca", s2: "abaac")
+//
+//func commovvnCharacterCount(s1: String, s2: String) -> Int {
+//
+//    var mutableFirstString = s1
+//    var counter = 0
+//    s2.characters.forEach {
+//        if let index = mutableFirstString.characters.indexOf($0) {
+//            counter = counter + 1
+//            mutableFirstString.removeAtIndex(index)
+//        }
+//    }
+//
+//    return counter
+//}
+//
+//commovvnCharacterCount(s1: "abaca", s2: "abaac")
