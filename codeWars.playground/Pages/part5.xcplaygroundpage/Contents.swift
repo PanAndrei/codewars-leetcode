ec78cc20e1289f9435647f2e4587b7e53205460a
PanAndrei <andrei_kmv@inbox.ru>
2021-11-09 20:28:21 +0300

    offset

--- codeWars.playground/Pages/part5.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part5.xcplaygroundpage/Contents.swift
@@ -2,6 +2,27 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
+//Given array of integers, remove each kth element from it.
 
-//: [Next](@next)
+//func extractEachKth(inputArray: [Int], k: Int) -> [Int] {
+//    guard k > 1 else {
+//        return []
+//    }
+//    var newArr = inputArray
+//    var answerArr = inputArray
+//    for i in 0 ... (newArr.count - 1) {
+//        if i == 0 {
+//            continue
+//        } else if (i + 1) % k == 0 {
+//            newArr[i] = 25
+//        }
+//    }
+//   answerArr =  newArr.filter { $0 != 25}
+//    return answerArr
+//}
+//
+//print(extractEachKth(inputArray: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], k: 3))
+//
+//func extractEachKth(inputArray: [Int], k: Int) -> [Int] {
+//    return inputArray.enumerated().filter { offset, _ in (offset + 1) % k != 0 }.map { _, value in value }
+//}
