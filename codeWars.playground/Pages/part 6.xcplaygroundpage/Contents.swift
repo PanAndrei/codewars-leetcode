6f6ca4bfd6bf9e05b2b91f7329c8054cbb01d156
PanAndrei <andrei_kmv@inbox.ru>
2021-11-17 13:16:46 +0300

    another chess

--- codeWars.playground/Pages/part 6.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part 6.xcplaygroundpage/Contents.swift
@@ -95,3 +95,92 @@ import UIKit
 //}
 //
 //solution(s: "aabbbcccerffs")
+
+
+//Given a position of a knight on the standard chessboard, find the number of different moves the knight can perform.
+//
+//The knight can move to a square that is two squares horizontally and one square vertically, or two squares vertically and one square horizontally away from it. The complete move therefore looks like the letter L. Check out the image below to see all valid moves for a knight piece that is placed on one of the central squares.
+// не работает там где 3 хода
+//func solution(cell: String) -> Int {
+//  var possibleMoves = 8
+//    var arrPositionUNicode = cell.map { Int($0.asciiValue!) }
+//
+//    if abs(arrPositionUNicode[0] - 97) < 1 || abs(arrPositionUNicode[0] - 104) < 1 {
+//        possibleMoves -= 4
+//    } else if abs(arrPositionUNicode[0] - 97) < 2 || abs(arrPositionUNicode[0] - 104) < 2 {
+//        possibleMoves -= 2
+//    }
+//
+//    if abs(arrPositionUNicode[1] - 49) < 1 || abs(arrPositionUNicode[1] - 56) < 1 {
+//        possibleMoves -= 4
+//    } else if abs(arrPositionUNicode[1] - 49) < 2 || abs(arrPositionUNicode[1] - 56) < 2 {
+//        possibleMoves -= 2
+//    }
+//
+//    if possibleMoves == 0 {
+//        possibleMoves = 2
+//    }
+//
+//    return possibleMoves
+//}
+
+// максимально тупое решение
+//func solution(cell: String) -> Int {
+//    let center = [100.5, 52.5]
+//    var possibleMoves = 0
+//    var arrPositionUNicode = cell.map { Double($0.asciiValue!) }
+//
+//    var lengh = sqrt(pow(arrPositionUNicode[0] - center[0], 2) + pow(arrPositionUNicode[1] - center[1], 2))
+//
+//    print(lengh)
+//
+//    switch lengh {
+//    case 4.31... :
+//        possibleMoves = 2
+//    case 3.81... :
+//        possibleMoves = 3
+//    case 3.5... :
+//        possibleMoves = 4
+//    case 2.54... :
+//        possibleMoves = 6
+//    default:
+//        possibleMoves = 8
+//    }
+//
+//    return possibleMoves
+//}
+//
+//solution(cell: "b4")
+
+//func isOnBoard(_ knight: [Int], _ move: [Int] ) -> Bool {
+//    let x = knight[0] + move[0]
+//    let y = knight[1] + move[1]
+//    return x >= 97 && x <= 104 && y >= 49 && y <= 56
+//}
+//
+//func solution(cell: String) -> Int {
+//    let knight = cell.utf8.map{Int($0)}
+//    let validOffsets = [[1,2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+//    return validOffsets.filter{isOnBoard(knight, $0)}.count
+//
+//}
+//// гениально
+//func solution(cell: String) -> Int {
+//    var r = [2, 3, 4, 6, 8]
+//    var arr = Array(cell)
+//    var c1 = min(arr[0].ascii! - 97, 104 - arr[0].ascii!);
+//    var c2 = min(arr[1].ascii! - 49, 56 - arr[1].ascii!);
+//    var d1 = min(c1, 2);
+//    var d2 = min(c2, 2);
+//    return r[Int(d1 + d2)];
+//}
+//extension Character {
+//    var isAscii: Bool {
+//        return unicodeScalars.first?.isASCII == true
+//    }
+//    var ascii: UInt32? {
+//        return isAscii ? unicodeScalars.first?.value : nil
+//    }
+//}
+//
+//solution(cell: "d4")
