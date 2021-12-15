dff5b7c4948d488bf27312ebec8febae03ee373c
PanAndrei <andrei_kmv@inbox.ru>
2021-12-15 14:30:15 +0300

    soop

---
+++ codeWars.playground/Pages/tinkoff.xcplaygroundpage/Contents.swift
@@ -0,0 +1,46 @@
+//: [Previous](@previous)
+
+//var a = 61429526
+//var b = 61430729
+//
+//var c = 2 * a + b
+//var d = 2 * b + a
+//
+//c < d
+//
+//print(c)
+///
+//let lengh =  945747
+//let coord =  130713
+//
+//let a = lengh - coord
+//let b = coord
+//
+//let dd = Double(a) / Double(b)
+//
+//let aa = 2 * a
+//let bb = 2 * b
+//
+//let ff = Double(aa) / Double(bb)
+//
+//var tt = [Int]()
+//
+//for i in 1 ... 100 {
+//    tt.append(aa * i)
+//}
+//
+//for i in 1 ... 1000 {
+//    var nj = bb * i
+//    if tt.contains(nj) {
+//        print(nj)
+//        break
+//    }
+//}
+//
+//var tto = 27711156
+///
+///
+
+
+//let high = 1000000000
+//let countVer = 100000
