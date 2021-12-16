632871d18adef64bf0fc6ac44ea96255f3046ac8
PanAndrei <andrei_kmv@inbox.ru>
2021-12-16 16:15:53 +0300

    гигаулучшение алгоритма

--- codeWars.playground/Pages/tinkoff.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/tinkoff.xcplaygroundpage/Contents.swift
@@ -44,3 +44,125 @@
 
 //let high = 1000000000
 //let countVer = 100000
+
+var answer = [Int]()
+//var kozel = 1234567
+//var a1 = Array([1,2, 3, 4, 6, 7, 8, 9])
+//var a2 = Array([3, 5, 7, 8, 9])
+//var a3 = Array([1, 2, 5, 7, 8, 9])
+//var a4 = Array([3, 4, 5, 6, 7, 8])
+//var a5 = Array([1, 3, 5, 7, 9])
+//var a6 = Array([3, 5, 6, 7])
+//var a7 = Array([2, 5, 6, 7, 8, 9])
+//var a8 = [9]
+//var a9 = Array([4, 8])
+//var a10 = Array([2, 5, 6, 7])
+//
+//answer.append(kozel % 4)
+//kozel /= 4
+//answer.append(kozel % 2)
+//kozel /= 2
+//answer.append(kozel % 1)
+//kozel /= 1
+//answer.append(kozel % 6)
+//kozel /= 6
+//answer.append(kozel % 4)
+//kozel /= 4
+//answer.append(kozel % 5)
+//kozel /= 5
+//answer.append(kozel % 6)
+//kozel /= 6
+//answer.append(kozel % 6)
+//kozel /= 6
+//answer.append(kozel % 5)
+//kozel /= 5
+//answer.append(kozel % 8)
+//kozel /= 8
+//
+//var normal = Array(answer.reversed())
+//print(normal)
+
+
+var a1 = Array([1,2, 3, 4, 6, 7, 8, 9].reversed())
+var a2 = Array([3, 5, 7, 8, 9].reversed())
+var a3 = Array([1, 2, 5, 7, 8, 9].reversed())
+var a4 = Array([3, 4, 5, 6, 7, 8].reversed())
+var a5 = Array([1, 3, 5, 7, 9].reversed())
+var a6 = Array([3, 5, 6, 7].reversed())
+var a7 = Array([2, 5, 6, 7, 8, 9].reversed())
+var a8 = [9]
+var a9 = Array([4, 8].reversed())
+var a10 = Array([2, 5, 6, 7].reversed())
+
+var all = [a1,a2,a3,a4,a5,a6,a7,a8,a9,a10]
+var ans = ""
+
+//for i in 0 ..< normal.count {
+//    ans += String(all[i][normal[i]])
+//}
+//print( ans)
+//
+//var q = 0
+//var w = 0
+//var e = 0
+//var r = 0
+//var t = 0
+//var y = 0
+//var u = 0
+//var i = 0
+//var o = 0
+//var p = 0
+//
+//
+//for k in 1 ... 147833 {
+//    answer.append(a1[q])
+//    answer.append(a2[w])
+//    answer.append(a3[e])
+//    answer.append(a4[r])
+//    answer.append(a5[t])
+//    answer.append(a6[y])
+//    answer.append(a7[u])
+//    answer.append(a8[i])
+//    answer.append(a9[o])
+//    answer.append(a10[p])
+//
+//    p += 1
+//    if p == a10.count {
+//        o += 1
+//        if o == a9.count {
+//            i += 1
+//            if i == a8.count {
+//                u += 1
+//                if u == a7.count {
+//                    y += 1
+//                    if y == a6.count {
+//                        t += 1
+//                        if t == a5.count {
+//                            r += 1
+//                            if r == a4.count {
+//                                e += 1
+//                                if e == a3.count {
+//                                    w += 1
+//                                    if w == a2.count {
+//                                        q += 1
+//                                        w = 0
+//                                    }
+//                                    e = 0
+//                                }
+//                                r = 0
+//                            }
+//                            t = 0
+//                        }
+//                        y = 0
+//                    }
+//                    u = 0
+//                }
+//                i = 0
+//            }
+//            o = 0
+//        }
+//        p = 0
+//    }
+//    print(answer)
+//    answer = []
+//}
