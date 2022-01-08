db09505551c0de06bc16b356f4a83d594924830f
PanAndrei <andrei_kmv@inbox.ru>
2022-01-08 14:43:45 +0300

    another 2 perfects

--- codeWars.playground/Pages/part12.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part12.xcplaygroundpage/Contents.swift
@@ -52,3 +52,38 @@ import Foundation
 //}
 //
 //solution(maxLength: 4, text: "The Fox asked the stork, 'How is the soup?'")
+
+//Elections are in progress!
+//
+//Given an array of the numbers of votes given to each of the candidates so far, and an integer k equal to the number of voters who haven't cast their vote yet, find the number of candidates who still have a chance to win the election.
+//
+//The winner of the election must secure strictly more votes than any other candidate. If two or more candidates receive the same (maximum) number of votes, assume there is no winner at all.
+
+//func solution(votes: [Int], k: Int) -> Int {
+//    if k < 1 && votes.filter { $0 == votes.max()! }.count > 1 {
+//        return 0
+//    } else if k < 1 && votes.filter { $0 == votes.max()! }.count > 0 {
+//        return 1
+//    }
+//    return votes.map { $0 + k }.filter { $0 > votes.max()! }.count
+//}
+//
+//solution(votes: [5, 1, 3, 4, 1], k: 0)
+
+//Given a positive integer number and a certain length, we need to modify the given number to have a specified length. We are allowed to do that either by cutting out leading digits (if the number needs to be shortened) or by adding 0s in front of the original number.
+
+//func solution(number: Int, width: Int) -> String {
+//    var tempString = String(repeating: "0", count: width).map { $0 }
+//    if width < String(number).count {
+//        for i in 0 ..< width {
+//            tempString[width - 1 - i] = String(number).map { $0 }[String(number).count - 1 - i]
+//        }
+//    } else {
+//        for i in 0 ..< String(number).count {
+//            tempString[width - 1 - i] = String(number).map { $0 }[String(number).count - 1 - i]
+//        }
+//    }
+//    return String(tempString)
+//}
+//
+//solution(number: 1234, width: 5)
