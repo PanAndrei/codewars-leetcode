141fd2cd65ac02d79f23b138d4fefd57a26b16f9
PanAndrei <andrei_kmv@inbox.ru>
2022-01-13 18:19:02 +0300

    experience

--- codeWars.playground/Pages/part12.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part12.xcplaygroundpage/Contents.swift
@@ -301,3 +301,30 @@ import Foundation
 //}
 //
 //solution(note: note)
+
+//Given two version strings composed of several non-negative decimal fields separated by periods ("."), both strings contain equal number of numeric fields. Return true if the first version is higher than the second version and false otherwise.
+//
+//The syntax follows the regular semver ordering rules:
+//
+//1.0.5 < 1.1.0 < 1.1.5 < 1.1.10 < 1.2.0 < 1.2.2
+//< 1.2.10 < 1.10.2 < 2.0.0 < 10.0.0
+//There are no leading zeros in any of the numeric fields, i.e. you do not have to handle inputs like 100.020.003 (it would instead be given as 100.20.3).
+
+//func solution(ver1: String, ver2: String) -> Bool {
+//    var answer = false
+//    let verr1 = Array(ver1.components(separatedBy: ".").reversed()).map { Int($0)! }
+//    let verr2 = Array(ver2.components(separatedBy: ".").reversed()).map { Int($0)! }
+//    
+//    for i in 0 ..< verr2.count {
+//        if verr1[i] > verr2[i] {
+//            answer = true
+//        } else if verr1[i] < verr2[i] {
+//            answer = false
+//        } else {
+//            continue
+//        }
+//    }
+//    return answer
+//}
+//
+//solution(ver1: "10", ver2: "9")
