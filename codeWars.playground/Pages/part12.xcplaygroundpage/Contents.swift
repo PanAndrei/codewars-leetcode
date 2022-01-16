a696e934399827bae0222dccf405ea56bc8d6a11
PanAndrei <andrei_kmv@inbox.ru>
2022-01-16 20:16:18 +0300

    new level

--- codeWars.playground/Pages/part12.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part12.xcplaygroundpage/Contents.swift
@@ -341,7 +341,7 @@ import Foundation
 //    var answer = ""
 //    let arrCiper = cipher.map { $0 }
 //    var letter = ""
-//    
+//
 //    for i in 0 ..< arrCiper.count {
 //        letter += String(arrCiper[i])
 //        if (97 ... 122).contains(Int(letter)!) {
@@ -353,3 +353,54 @@ import Foundation
 //}
 //
 //solution(cipher: "10197115121")
+
+//An alphanumeric ordering of strings is defined as follows: each string is considered as a sequence of tokens, where each token is a letter or a number (as opposed to an isolated digit, as is the case in lexicographic ordering). For example, the tokens of the string "ab01c004" are [a, b, 01, c, 004]. In order to compare two strings, we'll first break them down into tokens and then compare the corresponding pairs of tokens with each other (i.e. compare the first token of the first string with the first token of the second string, etc).
+//
+//Here is how tokens are compared:
+//
+//If a letter is compared with another letter, the usual alphabetical order applies.
+//A number is always considered less than a letter.
+//When two numbers are compared, their values are compared. Leading zeros, if any, are ignored.
+//If at some point one string has no more tokens left while the other one still does, the one with fewer tokens is considered smaller.
+//
+//If the two strings s1 and s2 appear to be equal, consider the smallest index i such that tokens(s1)[i] and tokens(s2)[i] (where tokens(s)[i] is the ith token of string s) differ only by the number of leading zeros. If no such i exists, the strings are indeed equal. Otherwise, the string whose ith token has more leading zeros is considered smaller.
+//
+//Here are some examples of comparing strings using alphanumeric ordering.
+//
+//"a" < "a1" < "ab"
+//"ab42" < "ab000144" < "ab00144" < "ab144" < "ab000144x"
+//"x11y012" < "x011y13"
+//Your task is to return true if s1 is strictly less than s2, and false otherwise.
+
+// не работает в полной мере
+//
+//func solution(s1: String, s2: String) -> Bool {
+//    var string1 = s1.map { Int($0.asciiValue!) }
+//    var string2 = s2.map { Int($0.asciiValue!) }
+//    var sum1 = 0
+//    var sum2 = 0
+//
+//    for i in 0 ..< string1.count {
+//        if string1[i] == 48 {
+//            string1[i] = 0
+//        }
+//    }
+//
+//    for i in 0 ..< string2.count {
+//        if string2[i] == 48 {
+//            string2[i] = 0
+//        }
+//    }
+//
+//    var answer: Bool
+//
+//    if string1.reduce(0, +) < string2.reduce(0, +) || string1.count < string2.count {
+//        answer = true
+//    } else {
+//        answer = false
+//    }
+//
+//    return answer
+// }
+//
+//solution(s1: "ab000144", s2: "x11y012")
