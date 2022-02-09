e7b42878e3207f3d442cfdab2e71c458bea9ef64
PanAndrei <andrei_kmv@inbox.ru>
2022-02-09 11:45:20 +0300

    some sorting

--- codeWars.playground/Pages/part15.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part15.xcplaygroundpage/Contents.swift
@@ -43,18 +43,18 @@ import Foundation
 //                continue
 //            }
 //
-//            var smallest = answer[i]
+//            let smallest = answer[i]
 //            if answer[j] < smallest {
 //                answer[i] = answer[j]
 //                answer[j] = smallest
 //            }
 //        }
 //    }
-//
+//print(answer)
 //    return answer
 //}
 //
-//solution(a: [-1, 150, 190, 170, -1, -1, 160, 180])
+//solution(a: [200, -1, 190, 170, -1, -1, 160, 180])
 
 // разобрать индексы деревьев
 //func solution(a: [Int]) -> [Int] {
@@ -64,4 +64,100 @@ import Foundation
 //        sorted.insert(-1, at: tree.offset)
 //    }
 //    return sorted
+//}
+
+//Given an array of strings, sort them in the order of increasing lengths. If two strings have the same length, their relative order must be the same as in the initial array.
+
+//let input = ["abc",
+//             "",
+//             "aaa",
+//             "a",
+//             "zz"]
+//
+//// отлично работает но двигает местами элементы одинаковой длины
+//func solution(inputArray: [String]) -> [String] {
+//    var answer = inputArray
+//
+//    for i in 0 ..< answer.count {
+//        for j in (i + 1) ..< answer.count {
+//            if answer[i].count > answer[j].count {
+//                answer.swapAt(i, j)
+//            }
+//        }
+//    }
+//
+//    return answer
+//}
+//
+//solution(inputArray: input)
+//
+//// answer
+//func solution(inputArray: [String]) -> [String] {
+//    return inputArray.sorted(by: {$0.count < $1.count})
+//}
+
+//You are given n rectangular boxes, the ith box has the length lengthi, the width widthi and the height heighti. Your task is to check if it is possible to pack all boxes into one so that inside each box there is no more than one another box (which, in turn, can contain at most one another box, and so on). More formally, your task is to check whether there is such sequence of n different numbers pi (1 ≤ pi ≤ n) that for each 1 ≤ i < n the box number pi can be put into the box number pi+1.
+//A box can be put into another box if all sides of the first one are less than the respective ones of the second one. You can rotate each box as you wish, i.e. you can swap its length, width and height if necessary.
+
+
+//let a = [1, 2, 3], b = [4, 3, 2]
+//
+//func checkBox(boxA: [Int], boxB: [Int]) -> Bool {
+//    let a = boxA.sorted()
+//    let b = boxB.sorted()
+//
+//    for i in 0 ..< a.count {
+//        if a[i] >= b[i] {
+//            return false
+//        }
+//    }
+//    return true
+//}
+//
+//checkBox(boxA: a, boxB: b)
+//
+//// работает на 12/ 13 но не учитывает что две маленькие в одну большую не влезут
+//func solution(length: [Int], width: [Int], height: [Int]) -> Bool {
+//    var multa = 0
+//    var ok = false
+//
+//    for i in 0 ..< length.count {
+//        let boxAA = [length[i], width[i], height[i]]
+//        for j in 0 ..< length.count {
+//            let boxBB = [length[j], width[j], height[j]]
+//            if checkBox(boxA: boxAA, boxB: boxBB) {
+//                ok = true
+//                continue
+//            }
+//        }
+//        if !ok {
+//            multa += 1
+//        }
+//        ok = false
+//    }
+//
+//    return multa > 1 ? false : true
+//}
+//
+//let length = [3, 1, 2], width = [3, 1, 2], height = [3, 2, 1]
+//
+//solution(length: length, width: width, height: height)
+//
+//// а все было легко
+//func solution(length: [Int], width: [Int], height: [Int]) -> Bool {
+//    var arr = [[Int]]()
+//
+//    for i in 0..<length.count {
+//        arr.append([length[i], width[i], height[i]].sorted())
+//    }
+//
+//    arr = arr.sorted(by: { $0[0] < $1[0] })
+//
+//    for i in 0..<arr.count-1 {
+//        if arr[i][0] >= arr[i+1][0] || arr[i][1] >= arr[i+1][1] || arr[i][2] >= arr[i+1][2] {
+//            return false
+//        }
+//    }
+//
+//    return true
 //}
