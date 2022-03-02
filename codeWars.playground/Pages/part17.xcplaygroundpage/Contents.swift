ede67851c17b2fc87f50a19fe2c81949b97ad014
PanAndrei <andrei_kmv@inbox.ru>
2022-03-02 20:00:43 +0300

    calendar

--- codeWars.playground/Pages/part17.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part17.xcplaygroundpage/Contents.swift
@@ -163,3 +163,24 @@ import Foundation
 //    if t <= 24*60 { count += 1 }
 //    return count
 //}
+
+//In an effort to be more innovative, your boss introduced a strange new tradition: the first day of every month you're allowed to work from home. You like this rule when the day falls on a Monday, because any excuse to skip rush hour traffic is great!
+//
+//You and your colleagues have started calling these months regular months. Since you're a fan of working from home, you decide to find out how far away the nearest regular month is, given that the currMonth has just started.
+//
+//For your convenience, here is a list of month lengths (from January to December, respectively):
+//
+//Month lengths: 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31.
+//Please, note that in leap years February has 29 days.
+//
+//func solution(currMonth: String) -> String {
+//    let dateFormatter = DateFormatter()
+//    dateFormatter.dateFormat = "MM-yyyy"
+//    let current = dateFormatter.date(from: currMonth)!
+//    let calendar = Calendar.current
+//    var next = current
+//    repeat {
+//        next = calendar.date(byAdding: .month, value: 1, to: next)!
+//    } while calendar.component(.weekday, from: next) != 2
+//    return dateFormatter.string(from: next)
+//}
