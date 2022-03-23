c7301d3b0cf157f993646ae47222a0bd91f6959b
PanAndrei <andrei_kmv@inbox.ru>
2022-03-23 17:20:55 +0300

    get and set

---
+++ codeWars.playground/Pages/angela.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+//import Foundation
+//
+//var width: Float = 1.5
+//var height: Float = 2.3
+//
+//var bucketsOfPaint: Int {
+//    get {
+//        return Int(ceil(width * height / 1.5))
+//    }
+//    set {
+//        print("this amount of paint can cover \(1.5 * Double(newValue)) sqr meters")
+//    }
+//}
+//
+//bucketsOfPaint
+//
+//bucketsOfPaint = 5
