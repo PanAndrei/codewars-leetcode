4420fd784e80712c326282e877e3118fd9bc9645
PanAndrei <andrei_kmv@inbox.ru>
2022-04-02 19:44:02 +0300

    + chess

--- codeWars.playground/Pages/part18.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part18.xcplaygroundpage/Contents.swift
@@ -296,3 +296,61 @@ import Darwin
 //}
 //
 //getPassword(withNums: 6)
+
+//John has always had trouble remembering chess game positions. To help himself with remembering, he decided to store game positions in strings. He came up with the following position notation:
+//
+//The notation is built for the current game position row by row from top to bottom, with '/' separating each row notation;
+//Within each row, the contents of each square are described from the leftmost column to the rightmost;
+//Each piece is identified by a single letter taken from the standard English names ('P' = pawn, 'N' = knight, 'B' = bishop, 'R' = rook, 'Q' = queen, 'K' = king);
+//White pieces are designated using upper-case letters ("PNBRQK") while black pieces use lowercase ("pnbrqk");
+//Empty squares are noted using digits 1 through 8 (the number of empty squares from the last piece);
+//Empty lines are noted as "8".
+//For example, for the initial position (shown in the picture below) the notation will look like this:
+//
+//"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+//
+//
+//
+//After the white pawn moves from e2 to e4, the notation will be as follows:
+//
+//"rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR"
+//
+//John has written down some positions using his notation, and now he wants to rotate the board 90 degrees clockwise and see what notation for the new board would look like. Help him with this task.
+
+//func solution(notation: String) -> String {
+//    var rotate  = [[String]](repeating: [], count: 8)
+//    var notation = notation.components(separatedBy: "/").map { str -> [String] in
+//        return Array(str).map { String($0) }
+//    }
+//    for r in 0..<notation.count {
+//        var c = 0
+//        while c < notation[r].count {
+//            if let number = Int(notation[r][c]) {
+//                notation[r][c...c] = ArraySlice<String>(repeating: "0", count: number)
+//                c += number
+//                continue
+//            }
+//            c += 1
+//        }
+//    }
+//    for i in 0...7 {
+//        var str = ""
+//        var countZero = 0
+//        for j in 0...7 {
+//            if notation[j][i] == "0" {
+//                countZero += 1
+//                continue
+//            }
+//            if countZero > 0 {
+//                str = "\(countZero)" + str
+//            }
+//            str = notation[j][i] + str
+//            countZero = 0
+//        }
+//        if countZero > 0 {
+//            str = "\(countZero)" + str
+//        }
+//        rotate[i] = Array(str).map { String($0) }
+//    }
+//    return rotate.map { $0.joined() }.joined(separator: "/")
+//}
