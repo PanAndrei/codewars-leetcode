1c17f64a4fd62783fe32870d27e68250663671f6
PanAndrei <andrei_kmv@inbox.ru>
2022-04-05 13:23:11 +0300

    ns regular pattern

--- codeWars.playground/Pages/part18.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part18.xcplaygroundpage/Contents.swift
@@ -393,3 +393,22 @@ import Darwin
 //    }
 //    return (0, "")
 //}
+
+//You've been invited to a job interview at a famous company that tests programming challenges. To evaluate your coding skills, they have asked you to parse a given problem's input given as an inputString string, and count the number of primitive type elements within it.
+//
+//The inputString can be either a primitive type variable or an array (possibly multidimensional) that contains elements of the primitive types.
+//A primitive type variable can be:
+//
+//an integer number;
+//a string, which is surrounded by " characters (note that it may contain any character except ");
+//a boolean, which is either true or false.
+//Return the total number of primitive type elements inside inputString.
+
+
+//func solution(inputString: String) -> Int {
+//    if inputString == "[]" { return 0 }
+//    let pattern = "(\".*?\")"
+//    let regex = try! NSRegularExpression(pattern: pattern, options: [])
+//    let str = regex.stringByReplacingMatches(in: inputString, options: [], range: NSRange(location: 0, length: inputString.count), withTemplate: " ")
+//    return str.components(separatedBy: ",").count
+//}
