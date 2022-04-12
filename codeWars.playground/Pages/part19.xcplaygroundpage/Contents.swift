031e52d48699a96fa37854df58cc43d6e31da933
PanAndrei <andrei_kmv@inbox.ru>
2022-04-12 11:00:54 +0300

    snake game

--- codeWars.playground/Pages/part19.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part19.xcplaygroundpage/Contents.swift
@@ -97,3 +97,112 @@ import Foundation
 //    }
 //    return grid
 //}
+
+
+//Your task is to imitate a turn-based variation of the popular "Snake" game
+//
+//You are given the initial configuration of the board and a list of commands which the snake follows one-by-one. The game ends if one of the following happens:
+//
+//the snake tries to eat its tail;
+//the snake tries to move out of the board;
+//it executes all the given commands.
+//Output the board configuration after the game ends.
+
+//func solution(gameBoard: [[Character]], commands: String) -> [[Character]] {
+//    let w = gameBoard[0].count
+//    let h = gameBoard.count
+//    var x = 0
+//    var y = 0
+//    var dx = 0
+//    var dy = 0
+//    for j in 0..<h {
+//        for i in 0..<w {
+//            let c = gameBoard[j][i]
+//            if c == Character(".") || c == Character("*") { continue }
+//            switch c {
+//            case Character("^"): x = i ; y = j ; dy = -1
+//            case Character("v"): x = i ; y = j ; dy = +1
+//            case Character("<"): x = i ; y = j ; dx = -1
+//            case Character(">"): x = i ; y = j ; dx = +1
+//            default: break
+//            }
+//        }
+//    }
+//    var snake = [(Int,Int)]()
+//    snake.append((x,y))
+//    
+//    func check(x: Int, _ y: Int) -> Bool {
+//        for s in snake {
+//            if s.0 == x && s.1 == y { return true }
+//        }
+//        return false
+//    }
+//    
+//    var ox = -10
+//    var oy = -10
+//    while true {
+//        var nx = x
+//        var ny = y
+//        if ox != x+1 && x+1 < w && gameBoard[y][x+1] == Character("*") {
+//            nx = x+1
+//        } else if ox != x-1 && x-1 >= 0 && gameBoard[y][x-1] == Character("*") {
+//            nx = x-1
+//        } else if oy != y+1 && y+1 < h && gameBoard[y+1][x] == Character("*") {
+//            ny = y+1
+//        } else if oy != y-1 && y-1 >= 0 && gameBoard[y-1][x] == Character("*") {
+//            ny = y-1
+//        } else {
+//            break
+//        }
+//        snake.insert((nx,ny), at: 0)
+//        ox = x
+//        oy = y
+//        x = nx
+//        y = ny
+//    }
+//    
+//    //
+//    for d in commands.map { $0 } {
+//        switch d {
+//        case Character("F"):
+//            let hh = snake.last!
+//            let x = hh.0 + dx
+//            let y = hh.1 + dy
+//            if x < 0 || x >= w || y < 0 || y >= h || check(x: x,y) {
+//                // dead
+//                var o = gameBoard
+//                for j in 0..<h {
+//                    for i in 0..<w {
+//                        o[j][i] = Character(check(x: i,j) ? "X" : ".")
+//                    }
+//                }
+//                return o
+//            } else {
+//                snake.removeFirst()
+//                snake.append((x,y))
+//            }
+//        case Character("L"):
+//            if dx == 1 { dx = 0 ; dy = -1 }
+//            else if dx == -1 { dx = 0 ; dy = 1 }
+//            else if dy == 1 { dx = 1 ; dy = 0 }
+//            else if dy == -1 { dx = -1 ; dy = 0 }
+//        case Character("R"):
+//            if dx == 1 { dx = 0 ; dy = 1 }
+//            else if dx == -1 { dx = 0 ; dy = -1 }
+//            else if dy == 1 { dx = -1 ; dy = 0 }
+//            else if dy == -1 { dx = 1 ; dy = 0 }
+//        default: break
+//        }
+//    }
+//    
+//    // alive
+//    var o = gameBoard
+//    for j in 0..<h {
+//        for i in 0..<w {
+//            o[j][i] = Character(check(x: i,j) ? "*" : ".")
+//        }
+//    }
+//    let hh = snake.last!
+//    o[hh.1][hh.0] = Character(dx == 1 ? ">" : dx == -1 ? "<" : dy == 1 ? "v" : "^")
+//    return o
+//}
