1ef67d10a89a334aca97ccbf6cdfb36d5fd4b9d9
PanAndrei <andrei_kmv@inbox.ru>
2022-06-28 10:53:45 +0300

    + leetcode

--- codeWars.playground/Pages/part20.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/part20.xcplaygroundpage/Contents.swift
@@ -8,14 +8,14 @@ import Foundation
 //print(arr.filter { $0.contains("ar")})
 
 
-struct CathalogDescription {
-    let description: String
-}
-
-
-var arrrr = [CathalogDescription(description: "gg ss"), CathalogDescription(description: "dd ww")]
-
-//var tt = arrrr.filter { $0.description.split(separator: " ").map { $0 }.filter { $0.contains("s")} }
-var tt = arrrr.filter { $0.description.range(of: "s") != nil }
-
-print(tt)
+//struct CathalogDescription {
+//    let description: String
+//}
+//
+//
+//var arrrr = [CathalogDescription(description: "gg ss"), CathalogDescription(description: "dd ww")]
+//
+////var tt = arrrr.filter { $0.description.split(separator: " ").map { $0 }.filter { $0.contains("s")} }
+//var tt = arrrr.filter { $0.description.range(of: "s") != nil }
+//
+//print(tt)
