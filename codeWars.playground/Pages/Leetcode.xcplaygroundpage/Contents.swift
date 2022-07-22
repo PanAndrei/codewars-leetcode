202bd88d96f3b443256936786f34197d15169e72
PanAndrei <andrei_kmv@inbox.ru>
2022-07-22 12:49:35 +0300

    + parentheses

--- codeWars.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ codeWars.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -47,3 +47,54 @@ import Foundation
 //        return answer
 //    }
 //}
+
+
+//Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+//
+//An input string is valid if:
+//
+//Open brackets must be closed by the same type of brackets.
+//Open brackets must be closed in the correct order.
+
+print("([)]".map { Character(String($0)).asciiValue! })
+
+    func isValid(_ s: String) -> Bool {
+        var arrString = s.map { String($0) }
+        
+        for _ in 0 ... 10_000 {
+            for i in 0 ..< arrString.count - 1 {
+                if arrString[i] ==
+            }
+        }
+        
+        
+        
+        
+        for _ in 0 ... 10_000 {
+            for i in 0 ..< arrString.count - 1 {
+                if arrString[i] == "." {
+                    continue
+                }
+                for j in i ..< arrString.count {
+                    if arrString[j] == "." {
+                        continue
+                    }
+                    if Int(Character(arrString[j]).asciiValue!) - Int(Character(arrString[i]).asciiValue!) == 1 ||
+                        Int(Character(arrString[j]).asciiValue!) - Int(Character(arrString[i]).asciiValue!) == 2 {
+                        arrString[j] = "."
+                        arrString[i] = "."
+                        continue
+                    } else {
+                        con
+                    }
+                }
+            }
+            if arrString.filter({ $0 == "." }).count == 0 {
+                return false
+            }
+        }
+        return arrString.filter { $0 == "." }.count == s.count
+    }
+
+isValid("([)]")
+
